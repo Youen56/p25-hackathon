02292c67ca78101d46e25a386d2634fd04e7c6fc
eliottdauresbouvet <eliottdaubou44@gmail.com>
2026-01-30 09:55:13 +0100

    main + animaux

---
+++ animals.py
@@ -0,0 +1,16 @@
+class sheep:
+    def __init__(self):
+        pass
+
+class wolf:
+    def __init__(self):
+        pass
+
+class grass:
+    def __init__(self):
+        pass
+
+def create_grass()
+    return [[0] * GRID_SIZE for _ in range(GRID_SIZE)]
+    
+    
